d6a878e0505c869cf4a8743a2ecab70ccb15de7e
vikivignesh301 <vikivignesh.1301@gmail.com>
2022-10-08 21:45:09 +0530

    first commit

---
+++ src/main/java/com/Property_FIle/ConfigurationReader.java
@@ -0,0 +1,63 @@
+package com.Property_FIle;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+	private Properties p;
+
+	public ConfigurationReader() throws IOException {
+		File f = new File(
+				"C:\\Users\\VIGNESH S\\eclipse-workspace\\FACEBOOK_PROJECT\\src\\main\\java\\com\\Property_FIle\\FaceBook.properties");
+		FileInputStream fi = new FileInputStream(f);
+		p = new Properties();
+		p.load(fi);
+	}
+
+	public String getBrowser() {
+		String browser = p.getProperty("browser");
+		return browser;
+	}
+
+	public String getUrl() {
+		String url = p.getProperty("url");
+		return url;
+
+	}
+
+	public String getUsername() {
+		String username = p.getProperty("username");
+		return username;
+	}
+
+	public String getPassword() {
+		String password = p.getProperty("password");
+		return password;
+	}
+
+	private String getDay() {
+		String day = p.getProperty("day");
+		return day;
+	}
+
+	private String getMonth() {
+		String month = p.getProperty("month");
+		return month;
+	}
+
+	private String getYear() {
+		String year = p.getProperty("year");
+		return year;
+	}
+
+	
+	
+	
+	
+	
+	
+}
